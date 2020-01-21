1a477df3f6d8fb5a8e08b99d7c9c7b56a26262f9
Александр Щербаков <tommynotbad@outlook.com>
2020-01-21 21:25:05 +0300

    added oledb conn to acces db

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Data.Sql;
+
 
 namespace datagridview_and_database
 {
@@ -15,6 +19,20 @@ namespace datagridview_and_database
         public Form1()
         {
             InitializeComponent();
+        }
+        OleDbConnection con = new OleDbConnection();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; 
+ Data Source=G:\My Project\DB.accdb;";    
+        }
+
+
+        private void refresh(object sender, EventArgs e)
+        {
+            string uodate_command = "INSERT INTO BAZA() VALUES ()"
+            OleDbCommand ref = new OleDbCommand(uodate_command, con );
         }
     }
 }
